5dac29233b2e58a8d7a3b63c229e16f5801afb3b
David Mitchell <monch1962@gmail.com>
2019-06-15 20:53:17 +1000

    Tweaked pending definition

--- cypress/support/step_definitions/lib/pending.js
+++ cypress/support/step_definitions/lib/pending.js
@@ -1,5 +1,10 @@
 /// <reference types="Cypress" />
 
+
+// This function exists to proxy steps that haven't been created yet.
+// It doesn't seem to be possible yet to actually mark a step as 'pending', so this will 
+// cause a test failure. That's good enough for now in that it doesn't let pending steps pass
 module.exports = () => {
-  this.pending()
+  // it.skip()
+  it.pending()
 }
