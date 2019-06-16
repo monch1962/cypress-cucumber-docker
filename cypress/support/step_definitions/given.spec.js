68198b4121c2159e98f9dc443378b10afaac73b3
David Mitchell <monch1962@gmail.com>
2019-06-17 07:34:47 +1000

    Code tidyup

--- cypress/support/step_definitions/given.spec.js
+++ cypress/support/step_definitions/given.spec.js
@@ -17,114 +17,114 @@ Given(
 )
 
 Given(
-    /^the element "([^"]*)?" is( not)* enabled$/,
-    // isEnabled
-    pending
+  /^the element "([^"]*)?" is( not)* enabled$/,
+  // isEnabled
+  pending
 )
 
 Given(
-    /^the element "([^"]*)?" is( not)* selected$/,
-    // checkSelected
-    pending
+  /^the element "([^"]*)?" is( not)* selected$/,
+  // checkSelected
+  pending
 )
 
 Given(
-    /^the checkbox "([^"]*)?" is( not)* checked$/,
-    // checkSelected
-    pending
+  /^the checkbox "([^"]*)?" is( not)* checked$/,
+  // checkSelected
+  pending
 )
 
 Given(
-    /^there is (an|no) element "([^"]*)?" on the page$/,
-    // checkElementExists
-    pending
+  /^there is (an|no) element "([^"]*)?" on the page$/,
+  // checkElementExists
+  pending
 )
 
 Given(
-    /^the title is( not)* "([^"]*)?"$/,
-    checkTitle
+  /^the title is( not)* "([^"]*)?"$/,
+  checkTitle
 )
 
 Given(
-    /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
-    // compareText
-    pending
+  /^the element "([^"]*)?" contains( not)* the same text as element "([^"]*)?"$/,
+  // compareText
+  pending
 )
 
 Given(
-    /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
-    // checkEqualsText
-    pending
+  /^the (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
+  // checkEqualsText
+  pending
 )
 
 Given(
-    /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
-    // checkContainsText
-    pending
+  /^the (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
+  // checkContainsText
+  pending
 )
 
 Given(
-    /^the (button|element) "([^"]*)?"( not)* contains any text$/,
-    // checkContainsAnyText
-    pending
+  /^the (button|element) "([^"]*)?"( not)* contains any text$/,
+  // checkContainsAnyText
+  pending
 )
 
 Given(
-    /^the (button|element) "([^"]*)?" is( not)* empty$/,
-    // checkIsEmpty
-    pending
+  /^the (button|element) "([^"]*)?" is( not)* empty$/,
+  // checkIsEmpty
+  pending
 )
 
 Given(
-    /^the page url is( not)* "([^"]*)?"$/,
-    // checkUrl
-    pending
+  /^the page url is( not)* "([^"]*)?"$/,
+  // checkUrl
+  pending
 )
 
 Given(
-    /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-    // checkProperty
-    pending
+  /^the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+  // checkProperty
+  pending
 )
 
 Given(
-    /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
-    // checkCookieContent
-    pending
+  /^the cookie "([^"]*)?" contains( not)* the value "([^"]*)?"$/,
+  // checkCookieContent
+  pending
 )
 
 Given(
-    /^the cookie "([^"]*)?" does( not)* exist$/,
-    // checkCookieExists
-    pending
+  /^the cookie "([^"]*)?" does( not)* exist$/,
+  // checkCookieExists
+  pending
 )
 
 Given(
-    /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
-    // checkDimension
-    pending
+  /^the element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
+  // checkDimension
+  pending
 )
 
 Given(
-    /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
-    // checkOffset
-    pending
-);
-
-Given(
-    /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
-    // resizeScreenSize
-    pending
+  /^the element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
+  // checkOffset
+  pending
 )
 
 Given(
-    /^I have closed all but the first (window|tab)$/,
-    // closeAllButFirstTab
-    pending
+  /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
+  // resizeScreenSize
+  pending
 )
 
 Given(
-    /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
-    // checkModal
-    pending
+  /^I have closed all but the first (window|tab)$/,
+  // closeAllButFirstTab
+  pending
+)
+
+Given(
+  /^a (alertbox|confirmbox|prompt) is( not)* opened$/,
+  // checkModal
+  pending
 )
--- cypress/support/step_definitions/then.spec.js
+++ cypress/support/step_definitions/then.spec.js
@@ -28,168 +28,168 @@ Then(
 )
 
 Then(
-    /^I expect that element "([^"]*)?" becomes( not)* visible$/,
-    // waitForVisible
-    pending
+  /^I expect that element "([^"]*)?" becomes( not)* visible$/,
+  // waitForVisible
+  pending
 )
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* within the viewport$/,
-    // checkWithinViewport
-    pending
+  /^I expect that element "([^"]*)?" is( not)* within the viewport$/,
+  // checkWithinViewport
+  pending
 )
 
 Then(
-    /^I expect that element "([^"]*)?" does( not)* exist$/,
-    // isExisting
-    pending
+  /^I expect that element "([^"]*)?" does( not)* exist$/,
+  // isExisting
+  pending
 )
 
 Then(
-    /^I expect that element "([^"]*)?"( not)* contains the same text as element "([^"]*)?"$/,
-    // compareText
-    pending
+  /^I expect that element "([^"]*)?"( not)* contains the same text as element "([^"]*)?"$/,
+  // compareText
+  pending
 )
 
 Then(
-    /^I expect that (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
-    // checkEqualsText
-    pending
-);
+  /^I expect that (button|element) "([^"]*)?"( not)* matches the text "([^"]*)?"$/,
+  // checkEqualsText
+  pending
+)
 
 Then(
-    /^I expect that (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
-    // checkContainsText
-    pending
-);
+  /^I expect that (button|element) "([^"]*)?"( not)* contains the text "([^"]*)?"$/,
+  // checkContainsText
+  pending
+)
 
 Then(
-    /^I expect that (button|element) "([^"]*)?"( not)* contains any text$/,
-    // checkContainsAnyText
-    pending
-);
+  /^I expect that (button|element) "([^"]*)?"( not)* contains any text$/,
+  // checkContainsAnyText
+  pending
+)
 
 Then(
-    /^I expect that (button|element) "([^"]*)?" is( not)* empty$/,
-    // checkIsEmpty
-    pending
-);
+  /^I expect that (button|element) "([^"]*)?" is( not)* empty$/,
+  // checkIsEmpty
+  pending
+)
 
 Then(
-    /^I expect that the url is( not)* "([^"]*)?"$/,
-    // checkURL
-    pending
-);
+  /^I expect that the url is( not)* "([^"]*)?"$/,
+  // checkURL
+  pending
+)
 
 Then(
-    /^I expect that the path is( not)* "([^"]*)?"$/,
-    // checkURLPath
-    pending
-);
+  /^I expect that the path is( not)* "([^"]*)?"$/,
+  // checkURLPath
+  pending
+)
 
 Then(
-    /^I expect the url to( not)* contain "([^"]*)?"$/,
-    // checkInURLPath
-    pending
-);
+  /^I expect the url to( not)* contain "([^"]*)?"$/,
+  // checkInURLPath
+  pending
+)
 
 Then(
-    /^I expect that the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-    // checkProperty
-    pending
-);
+  /^I expect that the( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+  // checkProperty
+  pending
+)
 
 Then(
-    /^I expect that the font( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
-    // checkFontProperty
-    pending
-);
+  /^I expect that the font( css)* attribute "([^"]*)?" from element "([^"]*)?" is( not)* "([^"]*)?"$/,
+  // checkFontProperty
+  pending
+)
 
 Then(
-    /^I expect that checkbox "([^"]*)?" is( not)* checked$/,
-    // checkSelected
-    pending
-);
+  /^I expect that checkbox "([^"]*)?" is( not)* checked$/,
+  // checkSelected
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* selected$/,
-    // checkSelected
-    pending
-);
+  /^I expect that element "([^"]*)?" is( not)* selected$/,
+  // checkSelected
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* enabled$/,
-    // isEnabled
-    pending
-);
+  /^I expect that element "([^"]*)?" is( not)* enabled$/,
+  // isEnabled
+  pending
+)
 
 Then(
-    /^I expect that cookie "([^"]*)?"( not)* contains "([^"]*)?"$/,
-    // checkCookieContent
-    pending
-);
+  /^I expect that cookie "([^"]*)?"( not)* contains "([^"]*)?"$/,
+  // checkCookieContent
+  pending
+)
 
 Then(
-    /^I expect that cookie "([^"]*)?"( not)* exists$/,
-    // checkCookieExists
-    pending
-);
+  /^I expect that cookie "([^"]*)?"( not)* exists$/,
+  // checkCookieExists
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
-    // checkDimension
-    pending
-);
+  /^I expect that element "([^"]*)?" is( not)* ([\d]+)px (broad|tall)$/,
+  // checkDimension
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
-    // checkOffset
-    pending
-);
+  /^I expect that element "([^"]*)?" is( not)* positioned at ([\d]+)px on the (x|y) axis$/,
+  // checkOffset
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" (has|does not have) the class "([^"]*)?"$/,
-    // checkClass
-    pending
-);
+  /^I expect that element "([^"]*)?" (has|does not have) the class "([^"]*)?"$/,
+  // checkClass
+  pending
+)
 
 Then(
-    /^I expect a new (window|tab) has( not)* been opened$/,
-    // checkNewWindow
-    pending
-);
+  /^I expect a new (window|tab) has( not)* been opened$/,
+  // checkNewWindow
+  pending
+)
 
 Then(
-    /^I expect the url "([^"]*)?" is opened in a new (tab|window)$/,
-    // checkIsOpenedInNewWindow
-    pending
-);
+  /^I expect the url "([^"]*)?" is opened in a new (tab|window)$/,
+  // checkIsOpenedInNewWindow
+  pending
+)
 
 Then(
-    /^I expect that element "([^"]*)?" is( not)* focused$/,
-    // checkFocus
-    pending
-);
+  /^I expect that element "([^"]*)?" is( not)* focused$/,
+  // checkFocus
+  pending
+)
 
 Then(
-    /^I wait on element "([^"]*)?"(?: for (\d+)ms)*(?: to( not)* (be checked|be enabled|be selected|be visible|contain a text|contain a value|exist))*$/,
-    // {
-    //     wrapperOptions: {
-    //         retry: 3,
-    //     },
-    // },
-    // waitFor
-    pending
-);
+  /^I wait on element "([^"]*)?"(?: for (\d+)ms)*(?: to( not)* (be checked|be enabled|be selected|be visible|contain a text|contain a value|exist))*$/,
+  // {
+  //     wrapperOptions: {
+  //         retry: 3,
+  //     },
+  // },
+  // waitFor
+  pending
+)
 
 Then(
-    /^I expect that a (alertbox|confirmbox|prompt) is( not)* opened$/,
-    // checkModal
-    pending
-);
+  /^I expect that a (alertbox|confirmbox|prompt) is( not)* opened$/,
+  // checkModal
+  pending
+)
 
 Then(
-    /^I expect that a (alertbox|confirmbox|prompt)( not)* contains the text "([^"]*)?"$/,
-    // checkModalText
-    pending
-);
+  /^I expect that a (alertbox|confirmbox|prompt)( not)* contains the text "([^"]*)?"$/,
+  // checkModalText
+  pending
+)
